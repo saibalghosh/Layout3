52f2807ea59ac24649d016efed7298098ee3e3b5
saibalghosh <saibal.kr.ghosh@gmail.com>
2013-05-17 16:29:35 -0400

    WIP Reps Settings View

---
+++ UCosmic.Web.Mvc/Controllers/RepresentativesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting.Web.Mvc;
+
+namespace UCosmic.Web.Mvc.Controllers
+{
+    public class RepresentativesController : Controller
+    {
+        //
+        // GET: /Representatives/
+
+        [GET("/representatives/settings")]
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
